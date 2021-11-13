285b18dfd79ffb0559d7510ddabf0fb03a69d2d6
Christian Eduardo Amaya Rivas <amayarivasc@gmail.com>
2021-11-12 18:40:55 -0600

    Fin del patron

--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -13,6 +13,7 @@ export interface ProductContextProps {
   counter: number;
   increaseBy: (value: number) => void;
   product: Product;
+  maxCount: number | undefined;
 }
 
 export interface ProductCardHOCProps {
@@ -32,4 +33,18 @@ export interface onChangeArgs {
 
 export interface ProductInCart extends Product {
   count: number;
+}
+
+export interface InitialValues {
+  count?: number;
+  maxCount?: number;
+}
+
+export interface ProductCardHandlers {
+  count: number;
+  isMaxCountReached: boolean;
+  maxCount?: number;
+  product: Product;
+  increaseBy: (value: number) => void;
+  reset: () => void;
 }
